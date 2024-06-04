a695787dfd3aa3f9245b1e3af0f2e02135b14f4e
ddeonseo <eunseo1370@gmail.com>
2024-06-04 20:00:05 +0900

    feat: fizzbuzz #1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,10 @@
+def fizz_buzz(n):
+    for i in range(1, n + 1):
+        output = ""
+        if i % 3 == 0:
+            output += "Fizz"
+        if i % 5 == 0:
+            output += "Buzz"
+        print(output or i)
 
+fizz_buzz(100)
